91b32c3581062345e719614e515cdf0f9d83d2b4
iyiolaidris <129879155+iyiolaidris@users.noreply.github.com>
2025-01-17 16:33:22 +0000

    Update CICD.sh

--- Method/CICD.sh
+++ Method/CICD.sh
@@ -2,12 +2,12 @@
 set -e
 
 APP_NAME=mywebapp-insecure
-VERSION=latest
+VERSION=1.0
 
 #BUILD: Clenaup old container/build image
 docker rm -f $APP_NAME 2>/dev/null || true
 docker build -t $APP_NAME:$VERSION . 
 
 #TEST: Run the container
-docker run -d -p 80:80 --name $APP_NAME:$VERSION webapp-nginx
+docker run -d -p 80:80 --name $APP_NAME:$VERSION webapp-nginx:latest
 
